611b17fea10673e35dd32ef062b5b7451963717a
Stephan Harbauer <stephanharbauer@gmail.com>
2019-03-13 21:40:10 +0100

    init commit

---
+++ src/main/java/de/idealo/robotcontrol/RobotControlApplication.java
@@ -0,0 +1,13 @@
+package de.idealo.robotcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotControlApplication.class, args);
+    }
+
+}
