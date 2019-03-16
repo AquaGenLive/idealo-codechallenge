c5e54a63761b8ecb76ae74d1919f15162a81aef8
Stephan Harbauer <stephanharbauer@gmail.com>
2019-03-16 13:43:35 +0100

    Fixed frontend errors

--- src/main/java/de/idealo/robotcontrol/ui/ControlFormElement.java
+++ src/main/java/de/idealo/robotcontrol/ui/ControlFormElement.java
@@ -2,9 +2,11 @@ package de.idealo.robotcontrol.ui;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ControlFormElement {
 
     private long id;
